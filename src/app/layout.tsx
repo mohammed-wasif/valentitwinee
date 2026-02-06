ce70d9b1ee9035f36af99cdead75cfa0da7f3cac
mohammed-wasif <83773232+mohammed-wasif@users.noreply.github.com>
2026-02-06 16:37:10 +0530

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "To Fathia",
+  description:
+    "Lets play a gamee",
+  keywords: [
+    "Valentine's card game",
+    "romantic proposal game",
+    "photo card challenge",
+    "Valentine's Day surprise",
+    "couples game",
+    "valentine's day game",
+    "proposal game",
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
